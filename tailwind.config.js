0ff6dfc68e69142b21ead82485c413c1dea9a646
ramezelbaroudy <ramezemadaiesec@gmail.com>
2024-10-08 17:12:35 +0200

    new page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                'custom-red': '#FF4B2B',
+                'custom-beige': '#F2E3DB',
+            }
+        },
+        plugins: [],
+    }
+}
+
